7da73775e573d8bfe65014f637a60c1c45637505
Dinura Sanmith <153044444+Sanmith2002@users.noreply.github.com>
2025-03-14 13:51:58 +0530

    add rates into user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
-// Newwwwww
+
 const userSchema = new mongoose.Schema({
     u_businessname: { type: String, required: true },
     u_email: { type: String, required: true, unique: true },
     u_phoneNo: { type: String, required: true },
     u_address: { type: String, required: true },
+    u_status: { type: String, enum: ["inactivate", "activate"], default: "inactivate" }, // New field to track approval status
+    u_pricingRate: { type: Number, default: null }, // Dynamic pricing rate
+    u_discountRate: { type: Number, default: null }, // Discount rate
+    u_advanceRate: { type: Number, default: null } // Advance rate
 }, { timestamps: true });
 
 // Automatically create a UserID as a primary key
