b04078ccdef44f9cce81a5fa4402409b2d2831e2
ChanakaWickramasingha <amanthachanaka223@gmail.com>
2025-03-19 10:13:01 +0530

    fixed bugs1.1./uploadImage/style/tailwind&css

--- frontend/src/components/ProductForm.jsx
+++ frontend/src/components/ProductForm.jsx
@@ -133,19 +133,19 @@ const ProductForm = () => {
           />
         </div>
         <div>
-          <label class="block text-sm font-medium text-white" htmlFor="images">Upload Images:</label>
+          <label className="block text-sm font-medium text-white" htmlFor="images">Upload Images:</label>
           <div className='"space-y-1 text-center"'>
-              <svg class="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+              <svg className="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                     <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
               <div className='flex text-sm text-gray-600'>
-                  <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                    <span class="">Upload a file</span>
-                      <input type="file" id="images" name="images" multiple accept="image/*" onChange={handleImageChange} required />
+                  <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                    <span className="">Upload a file</span>
+                      <input className='sr-only' type="file" id="images" name="images" multiple accept="image/*" onChange={handleImageChange} required />
                   </label>
-                  <p class="pl-1 text-white">or drag and drop</p>
+                  <p className="pl-1 text-white">or drag and drop</p>
               </div>
-              <p class="text-xs text-white">
+              <p className="text-xs text-white">
                     PNG, JPG, GIF up to 10MB
               </p>
           </div>
