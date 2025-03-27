75e280d5deb3012e843b1cefb69f0cbfd92a4ed2
ChanakaWickramasingha <amanthachanaka223@gmail.com>
2025-03-27 07:02:23 +0530

    implement validation/feature/

--- frontend/src/components/ProductForm.jsx
+++ frontend/src/components/ProductForm.jsx
@@ -4,13 +4,14 @@ import './ProductForm.css';
 
 const ProductForm = () => {
   const [product, setProduct] = useState({
-    manufacturingID: '',
     productName: '',
     ManufacturingCost: '',
+    sellingPrice: '',
     lowStockLevel: '',
     images: [],
   });
   const [message, setMessage] = useState('');
+  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -18,21 +19,56 @@ const ProductForm = () => {
       ...prev,
       [name]: value
     }));
+    // Clear error when user types
+    if (errors[name]) {
+      setErrors(prev => ({ ...prev, [name]: '' }));
+    }
   };
 
   const handleImageChange = (e) => {
     setProduct((prev) => ({
       ...prev,
-      images: Array.from(e.target.files)  // Store multiple selected files
+      images: Array.from(e.target.files)
     }));
   };
 
+  const validateForm = () => {
+    const newErrors = {};
 
+    //  selling price is not negative
+    if (product.sellingPrice < 0) {
+      newErrors.sellingPrice = 'Selling price cannot be negative';
+    }
 
+    //  manufacturing cost is not negative
+    if (product.ManufacturingCost < 0) {
+      newErrors.ManufacturingCost = 'Manufacturing cost cannot be negative';
+    }
+
+    // selling price should be higher than manufacutringCost
+    if (product.sellingPrice && product.ManufacturingCost &&
+        parseFloat(product.sellingPrice) <= parseFloat(product.ManufacturingCost)) {
+      newErrors.sellingPrice = 'Selling price must be higher than manufacturing cost';
+    }
+
+    //  stock level is not negative
+    if (product.lowStockLevel < 0) {
+      newErrors.lowStockLevel = 'Low stock level cannot be negative';
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Sending product data:', product);  // Add this line to log the product details
+
+    // Validate form before submission
+    if (!validateForm()) {
+      setMessage({ text: 'Please fix the errors in the form', type: "error" });
+      setTimeout(() => setMessage(null), 3000);
+      return;
+    }
 
     // Create FormData for file upload
     const formData = new FormData();
@@ -42,25 +78,28 @@ const ProductForm = () => {
     formData.append('lowStockLevel', product.lowStockLevel);
 
     product.images.forEach((image) => {
-      formData.append('images', image);  // Append each image to FormData
+      formData.append('images', image);
     });
 
     try {
-      const response = await axios.post('http://localhost:5000/api/products', formData,{
+      const response = await axios.post('http://localhost:5000/api/products', formData, {
         headers: {
-          'content-type':'multipart/form-data'
+          'content-type': 'multipart/form-data'
         }
       });
 
-      console.log('Response:', response.data);
       setMessage({ text: response.data.message, type: "success" });
-      setProduct({  productName: '', ManufacturingCost: '',sellingPrice: '', lowStockLevel: '', images: '', });// Clear form
-      setTimeout(() => setMessage(null), 3000); // auto hide msg after 3 sec
+      setProduct({
+        productName: '',
+        ManufacturingCost: '',
+        sellingPrice: '',
+        lowStockLevel: '',
+        images: []
+      });
+      setTimeout(() => setMessage(null), 3000);
     } catch (error) {
-
-      //console.error('Error adding product:', error.response ? error.response.data : error.message);
-
-      setMessage({ text:error.response?.data?.message || "failed to add product",
+      setMessage({
+        text: error.response?.data?.message || "Failed to add product",
         type: "error"
       });
       setTimeout(() => setMessage(null), 3000);
@@ -70,119 +109,116 @@ const ProductForm = () => {
   return (
     <div className='max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-20'>
       <h1 className="text-xl font-bold text-white capitalize dark:text-white">Add New Product</h1>
-      {message.text && (
-        <p className={` mt-2 p-2 text-center text-white ${message.type === "success" ? "bg-green-500" : "bg-red-500"}`}>
+      {message && (
+        <p className={`mt-2 p-2 text-center text-white ${message.type === "success" ? "bg-green-500" : "bg-red-500"}`}>
           {message.text}
         </p>
-        )
-      }
-      <form onSubmit={handleSubmit}  className="product-form">
-      <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
-        <div>
-          <label className='text-white dark:text-gray-200' htmlFor="productName">Product Name:</label>
-          <input
-            className='block w-full px-4 py-2 mt-2 text-gray-700
-                        bg-white border border-gray-300 rounded-md dark:bg-gray-800
-                        dark:text-gray-300 dark:border-gray-600 focus:border-blue-500
-                        dark:focus:border-blue-500 focus:outline-none focus:ring'
-            type="text"
-            id="productName"
-            name="productName"
-            value={product.productName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label  className='text-white dark:text-gray-200' htmlFor="ManufacturingCost">Manufacturing Cost:</label>
-          <input
-            className='block w-full px-4 py-2 mt-2 text-gray-700
-                        bg-white border border-gray-300 rounded-md dark:bg-gray-800
-                        dark:text-gray-300 dark:border-gray-600 focus:border-blue-500
-                        dark:focus:border-blue-500 focus:outline-none focus:ring'
-            type="number"
-            id="ManufacturingCost"
-            name="ManufacturingCost"
-            value={product.ManufacturingCost}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label className='text-white dark:text-gray-200' htmlFor="sellingPrice">Selling Price:</label>
-          <input
-             className='block w-full px-4 py-2 mt-2 text-gray-700
-                        bg-white border border-gray-300 rounded-md dark:bg-gray-800
-                        dark:text-gray-300 dark:border-gray-600 focus:border-blue-500
-                        dark:focus:border-blue-500 focus:outline-none focus:ring'
-            type="number"
-            id="sellingPrice"
-            name="sellingPrice"
-            value={product.sellingPrice}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label className='text-white dark:text-gray-200' htmlFor="lowStockLevel">Low Stock Level:</label>
-          <input
-             className='block w-full px-4 py-2 mt-2 text-gray-700
-                        bg-white border border-gray-300 rounded-md dark:bg-gray-800
-                        dark:text-gray-300 dark:border-gray-600 focus:border-blue-500
-                        dark:focus:border-blue-500 focus:outline-none focus:ring'
-            type="number"
-            id="lowStockLevel"
-            name="lowStockLevel"
-            value={product.lowStockLevel}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-white" htmlFor="images">Upload Images:</label>
-          <div className='space-y-1 text-center border-2 border-dashed border-gray-300 p-4 rounded-lg'>
+      )}
+      <form onSubmit={handleSubmit} className="product-form">
+        <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
+          <div>
+            <label className='text-white dark:text-gray-200' htmlFor="productName">Product Name:</label>
+            <input
+              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
+              type="text"
+              id="productName"
+              name="productName"
+              value={product.productName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label className='text-white dark:text-gray-200' htmlFor="ManufacturingCost">Manufacturing Cost:</label>
+            <input
+              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
+              type="number"
+              id="ManufacturingCost"
+              name="ManufacturingCost"
+              value={product.ManufacturingCost}
+              onChange={handleChange}
+              min="0"
+              step="0.01"
+              required
+            />
+            {errors.ManufacturingCost && (
+              <p className="mt-1 text-sm text-red-300">{errors.ManufacturingCost}</p>
+            )}
+          </div>
+          <div>
+            <label className='text-white dark:text-gray-200' htmlFor="sellingPrice">Selling Price:</label>
+            <input
+              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
+              type="number"
+              id="sellingPrice"
+              name="sellingPrice"
+              value={product.sellingPrice}
+              onChange={handleChange}
+              min="0"
+              step="0.01"
+              required
+            />
+            {errors.sellingPrice && (
+              <p className="mt-1 text-sm text-red-300">{errors.sellingPrice}</p>
+            )}
+          </div>
+          <div>
+            <label className='text-white dark:text-gray-200' htmlFor="lowStockLevel">Low Stock Level:</label>
+            <input
+              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
+              type="number"
+              id="lowStockLevel"
+              name="lowStockLevel"
+              value={product.lowStockLevel}
+              onChange={handleChange}
+              min="0"
+              required
+            />
+            {errors.lowStockLevel && (
+              <p className="mt-1 text-sm text-red-300">{errors.lowStockLevel}</p>
+            )}
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-white" htmlFor="images">Upload Images:</label>
+            <div className='space-y-1 text-center border-2 border-dashed border-gray-300 p-4 rounded-lg'>
               <svg className="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                  strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
               <div className='flex text-sm text-gray-600 '>
-                  <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                    <span className="">Upload a file</span>
-                    <input
-                          id="file-upload"
-                          type="file"
-                          multiple
-                          accept="image/*"
-                          onChange={handleImageChange}
-                          className="hidden"
-                    />
-                  </label>
-                  <p className="pl-1 text-white">or drag and drop</p>
+                <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                  <span className="">Upload a file</span>
+                  <input
+                    id="file-upload"
+                    type="file"
+                    multiple
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    className="hidden"
+                  />
+                </label>
+                <p className="pl-1 text-white">or drag and drop</p>
               </div>
               <p className="text-xs text-white">
-                    (PNG, JPG, GIF up to 10MB)
+                (PNG, JPG, GIF up to 10MB)
               </p>
               {product.images.length > 0 && (
-              <div className="mt-2 p-2 bg-gray-700 rounded-md">
-                <h3 className="text-white text-sm font-semibold">Selected Files:</h3>
-                <ul className="text-gray-300 text-xs">
-                  {product.images.map((file, index) => (
-                    <li key={index} className="truncate">{file.name}</li>
-                  ))}
-                </ul>
-              </div>
+                <div className="mt-2 p-2 bg-gray-700 rounded-md">
+                  <h3 className="text-white text-sm font-semibold">Selected Files:</h3>
+                  <ul className="text-gray-300 text-xs">
+                    {product.images.map((file, index) => (
+                      <li key={index} className="truncate">{file.name}</li>
+                    ))}
+                  </ul>
+                </div>
               )}
+            </div>
           </div>
-
         </div>
-      </div>
-      <div className='flex justify-end mt-6'>
-        <button className='px-6 py-2 leading-5 text-white transition-colors duration-200
-                            transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none
-                            focus:bg-gray-600'
-                            type="submit">Add Product</button>
-      </div>
-
+        <div className='flex justify-end mt-6'>
+          <button className='px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600'
+            type="submit">Add Product</button>
+        </div>
       </form>
     </div>
   );
