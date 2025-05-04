0d491ca1d9cc7d10004170c6f4dbf46ba93660b5
ChanakaWickramasingha <amanthachanaka223@gmail.com>
2025-05-04 12:59:51 +0530

    stocks are implemented1.1/stock

--- frontend/src/components/ProductForm.jsx
+++ frontend/src/components/ProductForm.jsx
@@ -368,10 +368,10 @@ const ProductForm = () => {
               <p className="mt-1 text-sm text-red-300">{errors.lowStockLevel}</p>
             )}
           </div>
+          <div>
           <label className='text-white dark:text-gray-200' htmlFor="quantity">Quantity:</label>
             <input
-              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
-              type="number"
+              className= 'block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
               id="quantity"
               name="quantity"
               value={product.quantity}
@@ -379,6 +379,7 @@ const ProductForm = () => {
               min="0"
               required
             />
+            </div>
         </div>
 
       )}
